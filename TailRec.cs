81e47c1ed802e9d119f5d15b6c40f4144adc5d9a
KOBAYASHI Naoya <kobayashi_na@mti.co.jp>
2021-02-08 12:36:37 +0900

    refactor: formatter

--- TailRec.cs
+++ TailRec.cs
@@ -3,33 +3,32 @@ using System.Collections.Generic;
 
 namespace Fib
 {
-public class TailRec<T>
-{
-
-  private readonly Func<TailRec<T>> _next;
-  private readonly bool _done;
-  private readonly T _result;
-
-  private TailRec(Func<TailRec<T>> next, bool done, T result)
+  public class TailRec<T>
   {
-    _next = next;
-    _done = done;
-    _result = result;
-  }
+    private readonly Func<TailRec<T>> _next;
+    private readonly bool _done;
+    private readonly T _result;
 
-  public T Get()
-  {
-    return this.Iterate(x => x._next()).Where(x => x._done).Select(x => x._result).First();
-  }
+    private TailRec(Func<TailRec<T>> next, bool done, T result)
+    {
+      _next = next;
+      _done = done;
+      _result = result;
+    }
 
-  public static TailRec<T> Call(Func<TailRec<T>> next)
-  {
-    return new TailRec<T>(next, false, default(T));
-  }
+    public T Get()
+    {
+      return this.Iterate(x => x._next()).Where(x => x._done).Select(x => x._result).First();
+    }
 
-  public static TailRec<T> Done(T result)
-  {
-    return new TailRec<T>(() => null, true, result);
+    public static TailRec<T> Call(Func<TailRec<T>> next)
+    {
+      return new TailRec<T>(next, false, default(T));
+    }
+
+    public static TailRec<T> Done(T result)
+    {
+      return new TailRec<T>(() => null, true, result);
+    }
   }
-}
 }
